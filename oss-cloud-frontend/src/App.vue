c4c95041d228e5e734ac3744ed4a69ad4a488cef
Aleksandar Nedaković <aleksaleks123@gmail.com>
2019-08-28 10:46:51 +0200

    Vue.js project and serverless project setup done
    Both apps can run on localhost (serverless offline plugin is added)

---
+++ oss-cloud-frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+@include md-register-theme(
+  "default",
+  (
+    primary: #1432a0,
+    // The primary color of your application
+      accent: #1432a0,
+    // The accent or secondary color
+  )
+);
+
+@import "~vue-material/dist/theme/all"; // Apply the theme
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
